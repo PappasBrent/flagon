4bfc24481c944241db7100ef6be3f3e58ce78076
Brent Pappas <pappasbrent@gmail.com>
2021-03-24 10:21:37 -0400

    Edge examples

--- examples/readme_examples.go
+++ examples/readme_examples.go
@@ -24,4 +24,18 @@ func Parse_labeled_graph() {
 		fmt.Printf("Parsed a node starting at line %v column %v\n",
 			node.Line, node.LeftColumn)
 	}
+
+	fmt.Println()
+
+	for label, edge := range graph.LabeledEdges {
+		fmt.Printf("Parsed an edge with label %v with top-left at"+
+			" %v:%v and bottom-right at %v:%v\n", label, edge.TopLine,
+			edge.LeftColumn, edge.BottomLine, edge.RightColumn)
+	}
+
+	for _, edge := range graph.Edges {
+		fmt.Printf("Parsed an edge with top-left at"+
+			" %v:%v and bottom-right at %v:%v\n", edge.TopLine,
+			edge.LeftColumn, edge.BottomLine, edge.RightColumn)
+	}
 }
